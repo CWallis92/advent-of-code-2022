2ad82a299f8a0420f26c0413d1b687f1fcae015e
Chris <chriswallis92@gmail.com>
2022-12-13 10:54:50 +0000

    Add notes to day 13

--- solutions/day-13.ts
+++ solutions/day-13.ts
@@ -11,40 +11,44 @@ const pairs = data.split("\n\n").map((pair) => {
   return { left, right };
 });
 
-let orderedPairs = 0;
-
+// Type for arbitrarily nested array
 interface NestedArray<T> extends Array<T | NestedArray<T>> {}
 
 const comparePackets = (
   left: NestedArray<number> | number,
   right: NestedArray<number> | number
 ): number => {
+  // Base case
   if (typeof left === "number" && typeof right === "number") {
     if (left === right) return 0;
 
     return right < left ? -1 : 1;
   }
 
+  // Nest the non-array value to continue comparison
   if (typeof left === "number") return comparePackets([left], right);
   if (typeof right === "number") return comparePackets(left, [right]);
 
-  if (Array.isArray(left) && Array.isArray(right)) {
-    let checkIndex = 0;
+  let checkIndex = 0;
 
-    while (checkIndex < Math.max(left.length, right.length)) {
-      if (left[checkIndex] === undefined) return 1;
-      if (right[checkIndex] === undefined) return -1;
+  while (checkIndex < Math.max(left.length, right.length)) {
+    // Check if one is shorter than the other, but only at the point where one array ends
+    if (left[checkIndex] === undefined) return 1;
+    if (right[checkIndex] === undefined) return -1;
 
-      const comparison = comparePackets(left[checkIndex], right[checkIndex]);
+    const comparison = comparePackets(left[checkIndex], right[checkIndex]);
 
-      if (comparison !== 0) return comparison;
+    // Order has been determined in recursive steps
+    if (comparison !== 0) return comparison;
 
-      checkIndex++;
-    }
-
-    return 0;
+    checkIndex++;
   }
+
+  // There should always be an order so this should never be reached
+  return 0;
 };
+
+let orderedPairs = 0;
 
 pairs.forEach((pair, pairIndex) => {
   const isOrdered = comparePackets(pair.left, pair.right) === 1;
